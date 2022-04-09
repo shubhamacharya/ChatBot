7f1c267ee830abd672e406229a5561d25bf5eddc
Shubham Acharya <shubhamacharya20@gmail.com>
2022-04-09 15:17:34 +0530

    feat: Added getUnanswered method in /admin to render all Unanswered question in admin panel.

--- app.py
+++ app.py
@@ -19,15 +19,17 @@ def predict():
     text = request.get_json().get("message")
     # TODO : check text validity
     global unanswered_question
+    
     response,text = get_response(text)
     unanswered_question.append(text)
-    
+    #unansweredWriteJSON(unanswered_question)
     message = {"answer":response}
     return jsonify(message)
 
 @app.route("/admin",methods=['GET'])
 def admin_get():
     tags = getTagList()
+    unanswered_question = getUnanswered()
     return render_template("admin.html",tags=tags,unanswered=unanswered_question)
 
 @app.route("/addQuestion",methods=['POST'])
@@ -43,6 +45,9 @@ def addQuestion_post():
     addQuestion(pattern,responses,tag)
     return redirect(url_for('admin_get'))
 
+@app.route("/unanswered",methods=['POST'])
+def unanswered_post():
+    unformattedPatterns = request.form[""]
 
 if __name__ == "__main__":
     app.run(debug=True)
