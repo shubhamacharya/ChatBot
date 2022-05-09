ffd1796048bbbcc61da6b739cac7767b9815927e
Shubham <shubhamacharya20@gmail.com>
2022-05-09 23:07:54 +0530

    fix: Timestamps added in logging for Admin's and Super Admin's.

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask,session,redirect,render_template,request,jsonify,url_for,flash,make_response
 from flask_cors import CORS
+from datetime import datetime
 from chat import get_response
 from werkzeug.security import generate_password_hash,check_password_hash
 from json_util import *
@@ -82,11 +83,12 @@ def predict():
 def admin_get():
     if 'user' in session:
         try:
-            createRequiredFiles('./unanswered.json')
+            createRequiredFiles('./logs.json')
             user = session['user']
             tags = getTagList()
             unanswered_question = getUnanswered(user)
             count = len(unanswered_question)
+            print(unanswered_question)
         except Exception as e:
             print(e)
         return render_template("admin.html",user=user,tags=tags,unanswered=unanswered_question,count=count)
@@ -95,44 +97,76 @@ def admin_get():
 
 @app.route("/addQuestion",methods=['POST'])
 def addQuestion_post():
+    user = session['user']
+    FILE = "./logs.json"
+
     unformattedPatterns = request.form["questions"]
     pattern = formatList(unformattedPatterns)
 
     unformattedResponses = request.form["answer"]
     responses = formatList(unformattedResponses)
-
     tag = request.form["newTag"]
+    
+    if addQuestion(pattern,responses,tag):
+        try:
+            file = open(FILE,"r+")
+            data = json.load(file)
+            
+            data["added"].append({
+                "questions" : pattern,
+                "response" : responses,
+                "tag" : tag,
+                "adminId" : user['email'],
+                "timeStamp" : datetime.now().strftime("%d-%b-%Y (%H:%M:%S.%f)")
+            })
 
-    addQuestion(pattern,responses,tag)
-    return redirect(url_for('admin_get'))
+            file = open(FILE,"w+")
+            file.seek(0)
+            json.dump(data,file,indent=4)
+            
+            flash("Question added Successfully...")
+            print("Question added Successfully...")     
+        except Exception as e:
+            print(e)
+        finally:
+            file.close()
+            session['user'] = user
+            return redirect(url_for('admin_get'))
 
 @app.route("/unanswered",methods=['POST'])
 def unanswered_post():
     user = session['user']
     question = request.form["question"]
     unformattedResponse = request.form["addAnswer"]
-    tags = request.form["addAnstags"]
-    FILE = "./unanswered.json"
-
+    tag = ""
+    if(request.form["addAnsTags"] != "nota"):
+        tag = request.form["addAnsTags"]
+    else:
+        tag = request.form["addNewAnsTags"]
+    
+    FILE = "./logs.json"
+    
     try:
         file = open(FILE,"r+")
         data = json.load(file)
 
         if user['role'] == 'superAdmin':
-            if addQuestion(question,unformattedResponse,tags,switch=True):
-                for i in data['question']:
+            if addQuestion(question,unformattedResponse,tag,switch=True):
+                for i in data['unanswered']:
                     if question in list(i.keys())[0]:
                         i['superAdminApproval'] = 1
-                        i['superAdminId'] = user['email']          
+                        i['superAdminId'] = user['email'] 
+                        i['superAdminTimeStamp'] = datetime.now().strftime("%d-%b-%Y (%H:%M:%S.%f)")    
             else:
                 flash("Question not added.")
         else: #For Normal Admins
-            for i in data['question']:
+            for i in data['unanswered']:
                 if question in list(i.keys())[0]:
                     i["response"] = unformattedResponse
-                    i["tag"] = tags
+                    i["tag"] = tag
                     i["adminId"] = user['email']
                     i[question] = 1
+                    i["adminTimeStamp"] = datetime.now().strftime("%d-%b-%Y (%H:%M:%S.%f)")
         
         file = open(FILE,"w+")
         file.seek(0)
@@ -140,12 +174,12 @@ def unanswered_post():
             
         flash("Question added Successfully...")
         print("Question added Successfully...")
-        session['user'] = user
-
+        
     except Exception as e:
         print(f"Error while writing to {FILE}.",e)
     finally:
         file.close()
+        session['user'] = user
         return redirect(url_for('admin_get'))
 
 @app.route("/api/tag",methods=['GET'])
@@ -157,7 +191,15 @@ def fetchTag():
 @app.route("/api/updateQuestion",methods=['POST'])
 def updateQuestion():
     req = request.get_json()
-    print(req)
+    
+    unformattedQuestion = list(req["pattern"])
+    unformattedAnswers = list(req["responses"])
+    tag = req["tag"]
+
+    patterns  = formatList(unformattedQuestion)
+    response = formatList(unformattedResponse)
+    
+    #Call to add updated question
     res = make_response(jsonify({"message":"Updated Question Successfully"}),200)
     return res
 
